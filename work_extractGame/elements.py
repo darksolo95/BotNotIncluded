efc45daacc0b091684556dc3dcdb0b4e65ea29aa
DarkSolo <136195106+darksolo95@users.noreply.github.com>
2025-11-26 21:51:51 +0800

    add colours to element data

--- work_extractGame/elements.py
+++ work_extractGame/elements.py
@@ -2,7 +2,9 @@ import collections
 import os.path as path
 
 import yaml
+import json
 
+import constant as constant
 import utils
 
 elements_base = path.join(
@@ -14,13 +16,41 @@ element_states = [
     "special",
 ]
 
-
+def get_substance_colours():
+    with open(constant.dict_PATH_EXTRACT_FILE['element'], 'r', encoding='utf-8') as file_elem:
+        data_elem = json.load(file_elem).get("elementTable", None)
+    dict_colours = {}
+    for _, ele in data_elem.items():
+        dict_colours[ele['tag']['Name']] = {}
+        substance = ele.get('substance', None)
+        if substance:
+            for colour_key in ['colour', 'uiColour', 'conduitColour']:
+                if colour_key in substance:
+                    colour_rgba = substance[colour_key]
+                    colour_hex = "#{:02x}{:02x}{:02x}{:02x}".format(colour_rgba['r'], colour_rgba['g'], colour_rgba['b'], colour_rgba['a'])
+                    dict_colours[ele['tag']['Name']][colour_key] = colour_hex
+        # utils.save_lua(path.join(utils.DIR_OUT, "Elements.lua"), dict_colours)
+    return dict_colours
+    
+    
 def main():
     data = []
     for state in element_states:
         with open(path.join(elements_base, f"{state}.yaml"), 'r') as f:
             data.extend(yaml.safe_load(f)["elements"])
     data = collections.OrderedDict({ele["elementId"]: ele for ele in data})
+    # 添加颜色信息
+    with open(constant.dict_PATH_EXTRACT_FILE['element'], 'r', encoding='utf-8') as file_elem:
+        data_elem = json.load(file_elem).get("elementTable", None)
+    for _, ele in data_elem.items():
+        if 'tag' in ele and 'Name' in ele['tag'] and ele['tag']['Name'] in data and 'substance' in ele:
+            substance = ele['substance']
+            if substance:
+                for colour_key in ['colour', 'uiColour', 'conduitColour']:
+                    if colour_key in substance:
+                        colour_rgba = substance[colour_key]
+                        colour_hex = "#{:02x}{:02x}{:02x}{:02x}".format(colour_rgba['r'], colour_rgba['g'], colour_rgba['b'], colour_rgba['a'])
+                        data[ele['tag']['Name']][colour_key] = colour_hex
 
     utils.save_lua(path.join(utils.DIR_OUT, "Elements.lua"), data)
 
